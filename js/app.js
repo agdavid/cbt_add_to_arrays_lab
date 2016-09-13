a27581bbd0bdeca6893657e29c5bdc2409d49c35
agdavid <antonio.g.david@gmail.com>
2016-09-13 09:05:15 -0400

    add JS conditional for adding item and not empty string

--- js/app.js
+++ js/app.js
@@ -6,10 +6,20 @@ function addItem() {
   var numberItemsDiv = document.getElementById('numberItems')
   var listItemsDiv = document.getElementById('listItems')
 
+  var newItem = document.getElementById('itemName').value;
+
+  if (newItem !== '') {
+    myList.push(newItem);
+  }
+
   if (myList.length === 0) {
     numberItemsDiv.innerHTML = myList.length.toString() + " items in the list";
     listItemsDiv.innerHTML = "There are no items to show!";
   } else {
-
+    numberItemsDiv.innerHTML = myList.length.toString() + " items in the list";
+    listItems();
   }
+};
+
+function listItems() {
 };
