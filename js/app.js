c5cad07ef9897b61be4c3fa5686fc80a622f952e
agdavid <antonio.g.david@gmail.com>
2016-09-13 09:00:23 -0400

    add JS if array is empty

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("ready!");
+
+myList = []
+
+function addItem() {
+  var numberItemsDiv = document.getElementById('numberItems')
+  var listItemsDiv = document.getElementById('listItems')
+
+  if (myList.length === 0) {
+    numberItemsDiv.innerHTML = myList.length.toString() + " items in the list";
+    listItemsDiv.innerHTML = "There are no items to show!";
+  } else {
+
+  }
+};
